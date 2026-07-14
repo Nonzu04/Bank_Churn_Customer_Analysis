1c8ce3801ebe784b67ed6003df981f6a5a90bf83
Nonzuleko Mbanjwa <mbanjwanonzuleko@gmail.com>
2026-07-14 16:49:45 +0200

    Add exploratory data analysis script for customer churn
    This script performs exploratory data analysis (EDA) on the cleaned bank customer dataset to identify patterns, trends, and potential factors influencing customer churn.

---
+++ SQL/04_Exploratory_Data_Analysis.sql
@@ -0,0 +1,22 @@
+/*
+=========================================================
+Script: 04_Exploratory_Data_Analysis.sql
+Project: Bank Customer Churn Analysis
+Author: Nonzuleko Mbanjwa
+Date: July 2026
+
+Description:
+This script performs exploratory data analysis (EDA)
+on the cleaned bank customer dataset to identify
+patterns, trends, and potential factors influencing
+customer churn.
+
+Objectives:
+- Understand customer demographics
+- Measure customer churn
+- Explore customer behaviour
+- Identify patterns associated with churn
+- Generate insights for business decision-making
+
+=========================================================
+*/
